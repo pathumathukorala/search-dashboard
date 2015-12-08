7063ce996f4d7cf3926096c471873e81e3246b6c
Pathum Athukorala <pathum.athukorala@ebuilder.com>
2015-12-08 11:08:51 +0530

    initial work

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name simpleAjaxApp
+ * @description
+ * # simpleAjaxApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('simpleAjaxApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simpleAjaxApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the simpleAjaxApp
+ */
+angular.module('simpleAjaxApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
